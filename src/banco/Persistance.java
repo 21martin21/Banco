c0f3ccfb9ea11cc6dd30c41b7ba56d6ab8cb783f
xusCrus <jesus.m.cruz.h@gmail.com>
2012-12-16 18:26:41 +0100

    Persistencia a mitad (ERRORRRR nunca se sube nada al repositorio si no funciona correctamente o está incompleto)

---
+++ src/banco/Persistance.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jesus
+ */
+public class Persistance {
+    
+    private String fileName = "";
+    private File file;
+    
+    public boolean save(ArrayList aguardar){
+        return false;
+    }
+    
+    public boolean load(){
+        return false;
+    }
+}
